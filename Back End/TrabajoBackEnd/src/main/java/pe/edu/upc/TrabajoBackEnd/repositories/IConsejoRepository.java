eae515363ea79c86088f94839467fde3a9a8bc14
Mario Valdivieso <g10vann1vch@gmail.com>
2024-04-04 14:45:40 -0500

    Implementacion de tabla consejo, con entidad, DTO, controlador, repositorio, interfaz e implementacion de servicio

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/repositories/IConsejoRepository.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/repositories/IConsejoRepository.java
@@ -1,2 +1,12 @@
-package pe.edu.upc.TrabajoBackEnd.repositories;public interface IConsejoRepository {
+package pe.edu.upc.TrabajoBackEnd.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.TrabajoBackEnd.entities.Consejo;
+
+import java.util.List;
+
+@Repository
+public interface IConsejoRepository extends JpaRepository<Consejo, Integer> {
+    public List<Consejo> findByTitulo(String titulo);
 }
--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/servicesinterfaces/IConsejoService.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/servicesinterfaces/IConsejoService.java
@@ -1,2 +1,13 @@
-package pe.edu.upc.TrabajoBackEnd.servicesinterfaces;public interface IConsejoService {
+package pe.edu.upc.TrabajoBackEnd.servicesinterfaces;
+
+import pe.edu.upc.TrabajoBackEnd.entities.Consejo;
+
+import java.util.List;
+
+public interface IConsejoService {
+    public void insert(Consejo consejo);
+    public List<Consejo> list();
+    public void delete(int id);
+    public Consejo listId(int id);
+    public List<Consejo> findbyTitulo(String titulo);
 }
