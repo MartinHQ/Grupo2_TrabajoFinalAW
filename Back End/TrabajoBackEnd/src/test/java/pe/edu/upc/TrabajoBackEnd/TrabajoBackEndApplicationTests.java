27e8fbd9e90e11be9fc7347332f37348864b368a
Martin Huillcahua Quispe <martinhuillcahuaquispe@gmail.com>
2024-04-04 09:47:13 -0500

    agregando las carpetas del trabajo

---
+++ Back End/TrabajoBackEnd/src/test/java/pe/edu/upc/TrabajoBackEnd/TrabajoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.TrabajoBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
