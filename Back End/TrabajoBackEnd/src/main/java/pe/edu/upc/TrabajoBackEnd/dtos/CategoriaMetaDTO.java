70117864893eeae6a1af462b00c9fa36780942fc
Fiorellab19 <135047166+Fiorellab19@users.noreply.github.com>
2024-04-05 14:14:18 -0500

    categoriameta services y DTO

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/dtos/CategoriaMetaDTO.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/dtos/CategoriaMetaDTO.java
@@ -1,4 +1,22 @@
 package pe.edu.upc.TrabajoBackEnd.dtos;
 
 public class CategoriaMetaDTO {
+    private  int idCategoriaMeta;
+    private String nombre;
+
+    public int getIdCategoriaMeta() {
+        return idCategoriaMeta;
+    }
+
+    public void setIdCategoriaMeta(int idCategoriaMeta) {
+        this.idCategoriaMeta = idCategoriaMeta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
