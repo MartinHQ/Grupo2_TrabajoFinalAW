a051b6fc2b26ad0be5eb995a95f9b786c85a5da0
Chrs¡ristian <u202123934@upc.edu.pe>
2024-04-05 12:10:34 -0500

    Cambios realizados

---
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/dtos/CategoriatranxDTO.java
@@ -0,0 +1,31 @@
+package pe.edu.upc.TrabajoBackEnd.dtos;
+
+public class CategoriatranxDTO {
+    private  int idCategoriatranx;
+    private String nombre;
+    private String descripcion;
+
+    public int getIdCategoriatranx() {
+        return idCategoriatranx;
+    }
+
+    public void setIdCategoriatranx(int idCategoriatranx) {
+        this.idCategoriatranx = idCategoriatranx;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/entities/Categoriatranx.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/entities/Categoriatranx.java
@@ -39,5 +39,4 @@ public class Categoriatranx {
         this.descripcion = descripcion;
     }
 
-    a
 }
