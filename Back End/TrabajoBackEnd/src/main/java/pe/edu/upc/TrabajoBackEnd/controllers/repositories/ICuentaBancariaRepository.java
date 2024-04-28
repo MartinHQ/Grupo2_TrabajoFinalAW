05856fcb4ccc2942957630e8f00f7f8992f160e4
Joshep Giovani Aquino Gutierrez <108606050+Josephgag3333@users.noreply.github.com>
2024-04-27 21:22:26 -0500

    Delete Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/controllers directory

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/controllers/repositories/ICuentaBancariaRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.TrabajoBackEnd.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.TrabajoBackEnd.entities.CuentaBancaria;
-
-@Repository
-public interface ICuentaBancariaRepository extends JpaRepository<CuentaBancaria, Integer> {
-}
