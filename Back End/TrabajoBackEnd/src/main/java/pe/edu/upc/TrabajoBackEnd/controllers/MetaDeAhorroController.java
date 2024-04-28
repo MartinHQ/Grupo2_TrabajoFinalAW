a7381472c41754c0a3736db0f1a1f36b51dfe3d2
Fiorellab19 <135047166+Fiorellab19@users.noreply.github.com>
2024-04-27 19:43:59 -0500

    se finalizo con el query reportemetascumplidas

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/controllers/MetaDeAhorroController.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/controllers/MetaDeAhorroController.java
@@ -3,11 +3,13 @@ package pe.edu.upc.TrabajoBackEnd.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.TrabajoBackEnd.dtos.CantMetaAhorroCumplidaDTO;
 import pe.edu.upc.TrabajoBackEnd.dtos.MetaDeAhorroDTO;
 import pe.edu.upc.TrabajoBackEnd.entities.MetaDeAhorro;
 import pe.edu.upc.TrabajoBackEnd.servicesinterfaces.IMetaDeAhorroService;
 import pe.edu.upc.TrabajoBackEnd.servicesinterfaces.IUsuarioService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,20 +18,37 @@ import java.util.stream.Collectors;
 @RequestMapping("/metadeahorro")
 public class MetaDeAhorroController {
     @Autowired
-    private IMetaDeAhorroService uS;
+    private IMetaDeAhorroService mS;
     @PostMapping
     public void insertarMetaDeAhorro(@RequestBody MetaDeAhorroDTO metaDeAhorroDTO) {
         ModelMapper model = new ModelMapper();
         MetaDeAhorro metaDeAhorro = model.map(metaDeAhorroDTO, MetaDeAhorro.class);
-        uS.insert(metaDeAhorro);
+        mS.insert(metaDeAhorro);
     }
     @GetMapping
     public List<MetaDeAhorroDTO> listarMetas() {
-        return uS.list().stream().map(y->{
+        return mS.list().stream().map(y->{
             ModelMapper m = new ModelMapper();
             return m.map(y, MetaDeAhorroDTO.class);
         }).collect(Collectors.toList());
     }
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id) { uS.delete(id); }
+    public void delete(@PathVariable("id") Integer id) { mS.delete(id); }
+
+    @GetMapping ("/reportemetascumplidas")
+    public List<CantMetaAhorroCumplidaDTO>listarpormetascumplidas(){
+        List<String[]> filaLista = mS.listarpormetascumplidas();
+        List<CantMetaAhorroCumplidaDTO> dtoLista = new ArrayList<>();
+
+        for (String[] columna : filaLista) {
+            CantMetaAhorroCumplidaDTO dto = new CantMetaAhorroCumplidaDTO();
+            dto.setNombre(columna[0]);
+            dto.setMeta_cumplida(Integer.parseInt(columna[1]));
+            dtoLista.add(dto);
+        }
+
+        return dtoLista;
+    }
+
+
 }
