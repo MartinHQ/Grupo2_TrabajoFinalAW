5fb6366a06b3ecc47bb88ca085de33443072f712
Mario Valdivieso <g10vann1vch@gmail.com>
2024-06-06 15:03:37 -0500

    Agregado componente Consejo, agregados botones de accesibilidad para Consejo y PF en html

--- Frontend/TrabajoFrontend/src/app/app.config.ts
+++ Frontend/TrabajoFrontend/src/app/app.config.ts
@@ -10,6 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     //para que funcione el httpClient
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideAnimationsAsync(),
   ],
 };
---
+++ Frontend/TrabajoFrontend/src/app/components/consejo/listar-consejo/listar-consejo.component.ts
@@ -0,0 +1,43 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { ConsejoService } from '../../../services/consejo.service';
+import { Consejo } from '../../../models/Consejo';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-listar-consejo',
+  standalone: true,
+  imports: [MatTableModule, 
+    MatPaginatorModule, 
+    MatIconModule, 
+    MatButtonModule,
+    RouterLink],
+  templateUrl: './listar-consejo.component.html',
+  styleUrl: './listar-consejo.component.css'
+})
+export class ListarConsejoComponent implements OnInit, AfterViewInit{
+
+  Columnas: string[] = ['codigo', 'titulo', 'descripcion', 'accion01'];
+  dataSource: MatTableDataSource<Consejo> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private CS: ConsejoService){}
+
+  ngAfterViewInit(): void {
+      this.dataSource.paginator = this.paginator;
+  }
+
+  ngOnInit(): void {
+    this.CS.list().subscribe((data)=>{
+      this.dataSource = new MatTableDataSource(data)
+      this.dataSource.paginator = this.paginator;
+    })
+
+    this.CS.getList().subscribe((data)=>{
+      this.dataSource = new MatTableDataSource(data)
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+}
