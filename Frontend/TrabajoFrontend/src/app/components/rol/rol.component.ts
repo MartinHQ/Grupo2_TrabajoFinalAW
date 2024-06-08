a3dc175afa870a190d155f6f63906aa08a1e01d4
Joshep Giovani Aquino Gutierrez <108606050+Josephgag3333@users.noreply.github.com>
2024-06-08 03:47:25 -0500

    Actualizado rol ts

--- Frontend/TrabajoFrontend/src/app/components/rol/rol.component.ts
+++ Frontend/TrabajoFrontend/src/app/components/rol/rol.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ListarRolComponent } from './listar-rol/listar-rol.component';
 
 @Component({
   selector: 'app-rol',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ListarRolComponent],
   templateUrl: './rol.component.html',
   styleUrl: './rol.component.css'
 })
 export class RolComponent {
-
+  constructor(public route: ActivatedRoute) {}
 }
