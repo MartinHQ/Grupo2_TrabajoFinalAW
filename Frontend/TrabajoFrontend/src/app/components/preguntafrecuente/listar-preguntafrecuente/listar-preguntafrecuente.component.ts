0e5d51326b14b5e0a9046cfa633128c697fcd7fd
MartinHQ <u201614196@upc.edu.pe>
2024-06-08 12:20:57 -0500

    implementado los cards

--- Frontend/TrabajoFrontend/src/app/components/preguntafrecuente/listar-preguntafrecuente/listar-preguntafrecuente.component.ts
+++ Frontend/TrabajoFrontend/src/app/components/preguntafrecuente/listar-preguntafrecuente/listar-preguntafrecuente.component.ts
@@ -1,25 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PreguntafrecuenteService } from '../../../services/preguntafrecuente.service';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { PreguntaFrecuente } from '../../../models/PreguntaFrecuente';
-import { MatPaginator } from '@angular/material/paginator';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-listar-preguntafrecuente',
   standalone: true,
   imports: [
-    MatPaginator,
     FormsModule,
-    MatTableModule,
     CommonModule,
     MatButton,
     MatIconModule,
-    RouterLink
+    RouterLink,
+    MatCardModule,
+    MatButtonModule,
   ],
   templateUrl: './listar-preguntafrecuente.component.html',
   styleUrl: './listar-preguntafrecuente.component.css',
@@ -27,31 +26,21 @@ import { RouterLink } from '@angular/router';
 export class ListarPreguntafrecuenteComponent implements OnInit {
   constructor(private pfS: PreguntafrecuenteService) {}
 
-  @ViewChild(MatPaginator) paginator?: MatPaginator;
-
+  listaPreguntasFrecuentes: PreguntaFrecuente[] = [];
   ngOnInit(): void {
     this.pfS.listar().subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
-      if (this.paginator) {
-        this.dataSource.paginator = this.paginator;
-      }
+      this.listaPreguntasFrecuentes = data;
     });
     this.pfS.getListaCambio().subscribe((data) => {
-      this.dataSource = new MatTableDataSource(data);
-      if (this.paginator) {
-        this.dataSource.paginator = this.paginator;
-      }
+      this.listaPreguntasFrecuentes = data;
     });
   }
 
-  eliminar(id:number){
-    this.pfS.eliminar(id).subscribe(()=>{
-      this.pfS.listar().subscribe((data)=>{
+  eliminar(id: number) {
+    this.pfS.eliminar(id).subscribe(() => {
+      this.pfS.listar().subscribe((data) => {
         this.pfS.setListaCambio(data);
       });
     });
   }
-
-  displayedColumns: string[] = ['codigo', 'pregunta', 'respuesta', 'acciones'];
-  dataSource: MatTableDataSource<PreguntaFrecuente> = new MatTableDataSource();
 }
