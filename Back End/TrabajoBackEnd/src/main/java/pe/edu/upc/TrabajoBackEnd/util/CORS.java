ece0c952183932529955bca0cdc17795df1ca60b
Martin Huillcahua Quispe <u201614196@upc.edu.pe>
2024-06-13 17:17:56 -0500

    cors backend actualizado

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/util/CORS.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/util/CORS.java
@@ -1,2 +1,44 @@
-package pe.edu.upc.TrabajoBackEnd.util;public class CORS {
+package pe.edu.upc.TrabajoBackEnd.util;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORS implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+        // chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
 }
