b34094e29acbfd0837cc009d29b067ac7ea9625c
MartinHQ <u201614196@upc.edu.pe>
2024-06-13 01:12:15 -0500

    funciona registrar, listar, editar y eliminar

--- Frontend/TrabajoFrontend/src/app/components/metadeahorro/creaedita-metadeahorro/creaedita-metadeahorro.component.ts
+++ Frontend/TrabajoFrontend/src/app/components/metadeahorro/creaedita-metadeahorro/creaedita-metadeahorro.component.ts
@@ -12,8 +12,8 @@ import { Usuario } from '../../../models/Usuario';
 import { TipometaService } from '../../../services/tipometa.service';
 import { UsuarioService } from '../../../services/usuario.service';
 import { MetadeahorroService } from '../../../services/metadeahorro.service';
-import { ActivatedRoute, RouterLink,Router } from '@angular/router';
-import { NgFor, NgIf,CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, Router, Params } from '@angular/router';
+import { NgFor, NgIf, CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -22,20 +22,33 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCalendar } from '@angular/material/datepicker';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-creaedita-metadeahorro',
   standalone: true,
-  imports: [RouterLink,ReactiveFormsModule,NgIf,MatFormFieldModule,MatDatepickerModule,
-           MatNativeDateModule,MatRadioModule,MatSelectModule,MatButtonModule,
-           MatInputModule,NgFor,NgIf,MatCalendar,MatCardModule,CommonModule],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    NgIf,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    NgFor,
+    MatCalendar,
+    MatCardModule,
+    CommonModule,
+  ],
   templateUrl: './creaedita-metadeahorro.component.html',
-  styleUrl: './creaedita-metadeahorro.component.css'
+  styleUrl: './creaedita-metadeahorro.component.css',
 })
 export class CreaeditaMetadeahorroComponent implements OnInit {
   form: FormGroup = new FormGroup({});
-  metaahorro: MetaDeAhorro=new MetaDeAhorro();
+  metaahorro: MetaDeAhorro = new MetaDeAhorro();
   listatipoMeta: TipoMeta[] = [];
   listaUsuarios: Usuario[] = [];
   edicion: boolean = false;
@@ -49,42 +62,49 @@ export class CreaeditaMetadeahorroComponent implements OnInit {
     private route: ActivatedRoute
   ) {}
 
- ngOnInit(): void {
-   this.route.params.subscribe((data)=>{
-    this.id=data['id'];
-    this.edicion=data['id']!=null;
-    this.Init();
-   })
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+      this.edicion = data['id'] != null;
+      this.Init();
+    });
 
-   this.form=this.formbuilder.group({
-     idmetadeahorro:['',Validators.required],
-     tiutlometa:['',Validators.required],
-     descripcion:['',Validators.required],
-     montoobjetivo:['',[Validators.required,Validators.min(1),Validators.pattern('^[0-9]*$')]],
-     fechalimite:['',Validators.required],
-     metacumplida:['',Validators.required],
-     usuarioid:['',Validators.required],
-     tipometaid:['',Validators.required],
-   });
+    this.form = this.formbuilder.group({
+      codigo: ['',],
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      monto: [
+        '',
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.pattern('^[0-9]*$'),
+        ],
+      ],
+      fecha: ['', Validators.required],
+      meta: ['', Validators.required],
+      usuarioid: ['', Validators.required],
+      tipometaid: ['', Validators.required],
+    });
 
-   this.mtS.listar().subscribe((data)=>{
-     this.listatipoMeta=data;
-   });
+    this.mtS.listar().subscribe((data) => {
+      this.listatipoMeta = data;
+    });
 
-   this.uS.list().subscribe((data)=>{
-    this.listaUsuarios=data;
-   });
- }
-  aceptar():void {
-    if(this.form.valid){
-      this.metaahorro.metadeahorro=this.form.value.idmetadeahorro;
-      this.metaahorro.titulo_meta=this.form.value.tiutlometa;
-      this.metaahorro.descripcion=this.form.value.descripcion;
-      this.metaahorro.monto_objetivo=this.form.value.montoobjetivo;
-      this.metaahorro.fecha_limite=this.form.value.fechalimite;
-      this.metaahorro.meta_cumplida=this.form.value.metacumplida;
-      this.metaahorro.usuario_id.usuario_id=this.form.value.usuarioid;
-      this.metaahorro.tipo_meta_id.idTipoMeta=this.form.value.tipometaid;
+    this.uS.list().subscribe((data) => {
+      this.listaUsuarios = data;
+    });
+  }
+  aceptar(): void {
+    if (this.form.valid) {
+      this.metaahorro.metadeahorro = this.form.value.codigo;
+      this.metaahorro.titulo_meta = this.form.value.nombre;
+      this.metaahorro.descripcion = this.form.value.descripcion;
+      this.metaahorro.monto_objetivo = this.form.value.monto;
+      this.metaahorro.fecha_limite = this.form.value.fecha;
+      this.metaahorro.meta_cumplida = this.form.value.meta === 'true';
+      this.metaahorro.usuario_id.usuario_id = this.form.value.usuarioid;
+      this.metaahorro.tipo_meta_id.idTipoMeta = this.form.value.tipometaid;
       if (this.edicion) {
         this.maS.modificar(this.metaahorro).subscribe(() => {
           this.maS.listar().subscribe((data) => {
@@ -92,7 +112,7 @@ export class CreaeditaMetadeahorroComponent implements OnInit {
           });
         });
       } else {
-        this.maS.registrar(this.metaahorro).subscribe((data) => {
+        this.maS.registrar(this.metaahorro).subscribe(() => {
           this.maS.listar().subscribe((data) => {
             this.maS.setListaCambio(data);
           });
@@ -100,31 +120,22 @@ export class CreaeditaMetadeahorroComponent implements OnInit {
       }
 
       this.router.navigate(['metadeahorro']);
-
     }
   }
- Init():void{
-  if (this.edicion) {
-   this.maS.listarId(this.id).subscribe((data)=>{
-     this.form= new FormGroup({
-      idmetadeahorro: new FormControl(data.metadeahorro),
-      tiutlometa: new FormControl(data.titulo_meta),
-      descripcion: new FormControl(data.descripcion),
-      montoobjetivo: new FormControl(data.monto_objetivo),
-      fechalimite:new FormControl(data.fecha_limite),
-      metacumplida: new FormControl(data.meta_cumplida ? 'true' : 'false'),
-      usuarioid: new FormControl(data.usuario_id.usuario_id),
-      tipometaid: new FormControl(data.tipo_meta_id.idTipoMeta),
-     });
-   });
+  Init(): void {
+    if (this.edicion) {
+      this.maS.listarId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          codigo: new FormControl(data.metadeahorro),
+          nombre: new FormControl(data.titulo_meta),
+          descripcion: new FormControl(data.descripcion),
+          monto: new FormControl(data.monto_objetivo),
+          fecha: new FormControl(data.fecha_limite),
+          meta: new FormControl(data.meta_cumplida ? 'true' : 'false'),
+          usuarioid: new FormControl(data.usuario_id.usuario_id),
+          tipometaid: new FormControl(data.tipo_meta_id.idTipoMeta),
+        });
+      });
+    }
   }
- }
- 
 }
-
-
-
-
-
-
-
--- Frontend/TrabajoFrontend/src/app/services/metadeahorro.service.ts
+++ Frontend/TrabajoFrontend/src/app/services/metadeahorro.service.ts
@@ -3,40 +3,39 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { MetaDeAhorro } from '../models/MetaDeAhorro';
 import { Subject } from 'rxjs';
- const base_url= environment.base
+const base_url = environment.base;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MetadeahorroService {
- private url=`${base_url}/metadeahorro`;
- private listacambio= new Subject<MetaDeAhorro[]>();
-  constructor(private http:HttpClient) { }
-  listar(){
+  private url = `${base_url}/metadeahorro`;
+  private listacambio = new Subject<MetaDeAhorro[]>();
+  constructor(private http: HttpClient) {}
+  listar() {
     return this.http.get<MetaDeAhorro[]>(this.url);
   }
 
-  registrar(mt: MetaDeAhorro){
-    return this.http.post(this.url,mt)
+  registrar(mt: MetaDeAhorro) {
+    return this.http.post(this.url, mt);
   }
-  
-  getListaCambio(){
+
+  getListaCambio() {
     return this.listacambio.asObservable();
   }
 
-  setListaCambio(listanueva:MetaDeAhorro[]){
+  setListaCambio(listanueva: MetaDeAhorro[]) {
     this.listacambio.next(listanueva);
   }
 
-  listarId(id:number){
-   return this.http.get<MetaDeAhorro>(`${base_url}/${id}`)
+  listarId(id: number) {
+    return this.http.get<MetaDeAhorro>(`${this.url}/${id}`);
   }
 
-  modificar(mt:MetaDeAhorro){
-    return this.http.put(this.url,mt)
+  modificar(mt: MetaDeAhorro) {
+    return this.http.put(this.url, mt);
   }
 
-  eliminar(id:number){
-    return this.http.delete(`${this.url}/${id}`)
+  eliminar(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
   }
-
 }
