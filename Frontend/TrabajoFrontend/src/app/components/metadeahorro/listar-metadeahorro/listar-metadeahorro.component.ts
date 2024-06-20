11261aaf5f8cc565384142808f20ef2ab8c9995e
Fiorellab19 <135047166+Fiorellab19@users.noreply.github.com>
2024-06-20 01:11:06 -0500

    se modifico el listar de meta de ahorro y se agrego un componente que muestra un dialogo para cuando se presione el boton del ojo

--- Frontend/TrabajoFrontend/src/app/components/metadeahorro/listar-metadeahorro/listar-metadeahorro.component.ts
+++ Frontend/TrabajoFrontend/src/app/components/metadeahorro/listar-metadeahorro/listar-metadeahorro.component.ts
@@ -6,15 +6,31 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MetaDeAhorro } from '../../../models/MetaDeAhorro';
 import { MetadeahorroService } from '../../../services/metadeahorro.service';
-import { NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.component';
+import { Observable } from 'rxjs';
+import { Usuario } from '../../../models/Usuario';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from '../../../services/login.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { VerDetalleDialogoComponent } from '../ver-detalle-dialogo/ver-detalle-dialogo.component';
 @Component({
   selector: 'app-listar-metadeahorro',
   standalone: true,
   imports: [
+    FormsModule,
+    CommonModule,
     MatPaginatorModule,
     MatTableModule,
+    MatButton,
     MatIconModule,
     RouterLink,
     MatButtonModule,
@@ -22,29 +38,30 @@ import { ConfirmDialogComponent } from '../../confirm-dialog/confirm-dialog.comp
     NgFor,
     ConfirmDialogComponent,
     MatDialogModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatProgressBarModule,
+    MatChipsModule,
+    MatSort,
+    MatSortModule
   ],
   templateUrl: './listar-metadeahorro.component.html',
   styleUrl: './listar-metadeahorro.component.css',
 })
-export class ListarMetadeahorroComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = [
-    'codigo',
-    'titulo',
-    'descripcion',
-    'montoobjetivo',
-    'fechalimite',
-    'tipometanombre',
-    'usuarionombre',
-    'metacumplida',
-    'accion01',
-    'accion02',
-  ];
-
+export class ListarMetadeahorroComponent implements OnInit {
   datasource: MatTableDataSource<MetaDeAhorro> = new MatTableDataSource();
-
+  originaldata: MetaDeAhorro[]=[];
+  obs?: Observable<any>;
+  usuariologeado: Usuario= new Usuario()
+  
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private mS: MetadeahorroService, private dialog: MatDialog) {}
+  constructor(private mS: MetadeahorroService, private dialog: MatDialog,
+              private snackBar:MatSnackBar, private ls: LoginService,
+  ) {}
+
+  mensaje: string='';
 
   openDialog(id: number): void {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
@@ -56,26 +73,36 @@ export class ListarMetadeahorroComponent implements OnInit, AfterViewInit {
       }
     });
   }
-  ngOnInit(): void {
-    this.mS.listar().subscribe((data) => {
-      this.datasource.data = data;
-      this.datasource.paginator = this.paginator;
-    });
+  openDetailDialog(element:MetaDeAhorro):void{
+    this.dialog.open(VerDetalleDialogoComponent,{data:element});
+  }
 
-    this.mS.getListaCambio().subscribe((data) => {
-      this.datasource.data = data;
-      this.datasource.paginator = this.paginator;
-    });
+  ngOnInit(): void {
+    this.usuariologeado= this.ls.getCurrentUser()!;
+    if(this.usuariologeado && this.usuariologeado.usuario_id){
+      this.mS.listarporusuarioactivo(this.usuariologeado.usuario_id).subscribe(data=>{
+        this.datasource.data=data;
+        this.datasource.paginator=this.paginator;
+        this.obs=this.datasource.connect();
+      });
+      this.mS.getListaCambio().subscribe(data=>{
+        this.datasource.data=data;
+        this.datasource.paginator=this.paginator;
+        this.obs=this.datasource.connect();
+      });
+    }
   }
 
   eliminar(id: number) {
-    this.mS.eliminar(id).subscribe((data) => {
-      this.mS.listar().subscribe((data) => {
-        this.mS.setListaCambio(data);
-      });
+    this.mS.eliminar(id).subscribe(() => {
+      this.usuariologeado=this.ls.getCurrentUser()!;
+      if(this.usuariologeado && this.usuariologeado.usuario_id){
+        this.mS.listarporusuarioactivo(this.usuariologeado.usuario_id).subscribe((data)=>{
+          this.mS.setListaCambio(data);
+          this.mensaje='Meta de Ahorro eliminada correctamente';
+          this.snackBar.open(this.mensaje,'Cerrar',{duration:2000});
+        });
+      }
     });
-  }
-  ngAfterViewInit(): void {
-    this.datasource.paginator = this.paginator;
   }
 }
