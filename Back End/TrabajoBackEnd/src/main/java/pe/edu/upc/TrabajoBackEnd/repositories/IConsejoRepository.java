1c557c2152fb08b81d9c20372dbf1fd0f524dd5e
MartinHQ <u201614196@upc.edu.pe>
2024-06-22 16:29:40 -0500

    cambios de query

--- Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/repositories/IConsejoRepository.java
+++ Back End/TrabajoBackEnd/src/main/java/pe/edu/upc/TrabajoBackEnd/repositories/IConsejoRepository.java
@@ -18,19 +18,17 @@ public interface IConsejoRepository extends JpaRepository<Consejo, Integer> {
 
     @Query(value = "SELECT c.id_consejo, c.titulo, c.descripcion \n" +
             "FROM consejo c \n" +
-            "WHERE LOWER(c.titulo) IN ( \n" +
-            "    SELECT LOWER(categoria) \n" +
-            "    FROM ( \n" +
-            "        SELECT ct.nombre as categoria, MAX(t.monto_transaccion) as maximo_monto \n" +
-            "\t\t FROM Transaccion t \n" +
-            "\t\t JOIN categoria_tranx ct ON t.categoria_id = ct.id_categoriatranx \n" +
-            "\t\t JOIN usuario u ON u.usuario_id = t.usuario_id \n" +
-            "WHERE t.es_ingreso_transaccion = false \n" +
-            "AND t.usuario_id = :id_usuario \n" +
-            "AND t.fecha_transaccion BETWEEN :date1 AND :date2 \n" +
-            "GROUP BY ct.nombre \n" +
-            "    ) AS subconsulta \n" +
-            ");", nativeQuery = true)
+            "         JOIN ( \n" +
+            "    SELECT ct.nombre as categoria, MAX(t.monto_transaccion) as maximo_monto \n" +
+            "    FROM Transaccion t \n" +
+            "             JOIN categoria_tranx ct ON t.categoria_id = ct.id_categoriatranx \n" +
+            "             JOIN usuario u ON u.usuario_id = t.usuario_id \n" +
+            "    WHERE t.es_ingreso_transaccion = false \n" +
+            "      AND t.usuario_id = :id_usuario \n" +
+            "      AND t.fecha_transaccion BETWEEN :date1 AND :date2 \n" +
+            "    GROUP BY ct.nombre \n" +
+            ") AS subconsulta \n" +
+            "              ON LOWER(c.titulo) LIKE '%' || LOWER(subconsulta.categoria) || '%';", nativeQuery = true)
     public List<Consejo> listarConsejoPorMaxMontoCategoria(@Param("date1")LocalDate date1,
                                                            @Param("date2")LocalDate date2,
                                                            @Param("id_usuario")int id_usuario);
--- Frontend/TrabajoFrontend/src/app/services/consejo.service.ts
+++ Frontend/TrabajoFrontend/src/app/services/consejo.service.ts
@@ -1,38 +1,54 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { Consejo } from '../models/Consejo';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-const base_url = environment.base
+const base_url = environment.base;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsejoService {
-  private url =`${base_url}/consejos`
-  private updatedlistConsejos = new Subject<Consejo[]>()
-  constructor(private http:HttpClient) { }
+  private url = `${base_url}/consejos`;
+  private updatedlistConsejos = new Subject<Consejo[]>();
+  constructor(private http: HttpClient) {}
 
-  list(){
-    return this.http.get<Consejo[]>(this.url)
+  list() {
+    return this.http.get<Consejo[]>(this.url);
   }
-  insert(c: Consejo){
-    return this.http.post(this.url,c)
+  insert(c: Consejo) {
+    return this.http.post(this.url, c);
   }
-  setList(newlist: Consejo[]){
-    this.updatedlistConsejos.next(newlist)
+  setList(newlist: Consejo[]) {
+    this.updatedlistConsejos.next(newlist);
   }
-  getList(){
-    return this.updatedlistConsejos.asObservable()
+  getList() {
+    return this.updatedlistConsejos.asObservable();
   }
-  listId(id: number){
-    return this.http.get<Consejo>(`${this.url}/${id}`)
+  listId(id: number) {
+    return this.http.get<Consejo>(`${this.url}/${id}`);
   }
-  update(c: Consejo){
-    return this.http.put(this.url, c)
+  update(c: Consejo) {
+    return this.http.put(this.url, c);
   }
-  eliminar(id: number){
-    return this.http.delete(`${this.url}/${id}`)
+  eliminar(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  listConsejosByCategoria(
+    fechaInicio: Date,
+    fechaFin: Date,
+    idUsuario: number
+  ): Observable<Consejo[]> {
+    const formatDate = (date: Date) => date.toISOString().split('T')[0];
+
+    let params = new HttpParams()
+      .set('date1', formatDate(fechaInicio))
+      .set('date2', formatDate(fechaFin))
+      .set('id_usuario', idUsuario.toString());
+
+    return this.http.get<Consejo[]>(`${this.url}/consejos-por-categoria`,
+      { params }
+    );
   }
 }
