d45b1a49fac9cf6fbe52d429835aec9511cce4d0
MartinHQ <u201614196@upc.edu.pe>
2024-06-22 22:17:00 -0500

    eliminar Landing page

--- LandingPage/java/scriptcarga.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load',function(){
-    document.getElementById("loader").classList.toggle("loader2")
-
-})
--- LandingPage/java/scriptpago.js
+++
@@ -1,7 +0,0 @@
-const select = document.querySelector('#select');
-const opciones = document.querySelector('#opciones');
-
-select.addEventListener('click', () => {
-	select.classList.toggle('active');
-	opciones.classList.toggle('active');
-});
